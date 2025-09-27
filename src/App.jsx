f146e95db74970ad69afa92b61c40c45324866e0
Mayank-Kapadane <mayank.kapadane2005@gmail.com>
2025-09-27 16:18:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Navbar} from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App;
